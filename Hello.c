ef69d3aa820b69c4c3e9ac820e487ade97b052e0
saurabh-xo <saurabhsatpute7@gmail.com>
2020-10-21 16:01:50 +0530

    Hello World in C

---
+++ Hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+Maintainded by : Saurabh
+*/
+
+int main (int argc, chaaar* argv[])
+{
+	printf("Hello World in C\n"); //print the hello world in c on console
+
+return (0);
+}
